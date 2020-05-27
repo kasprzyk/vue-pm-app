8a5ab26bab2061c92d51b6b903c13ca9c3682b32
kasprzyk <piotkas1@gmail.com>
2020-05-27 16:32:32 +0200

    AppDrag added

---
+++ src/components/AppDrag.vue
@@ -0,0 +1,19 @@
+<template>
+  <div draggable @dragstart.self="onDrag" @dragover.prevent @dragenter.prevent>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    onDrag(e) {
+      e.dataTransfer.effectAllowed = 'move';
+      e.dataTransfer.dropEffect = 'move';
+      e.dataTransfer.setData('payload', JSON.stringify(this.transferData));
+    }
+  }
+};
+</script>
+
+<style lang="css" scoped></style>
